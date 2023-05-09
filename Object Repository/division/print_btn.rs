1dd939a93632ac1351ad8bac1a159421b9feb97a
M Gilang Akbar Hikmawan <akbargilang98@gmail.com>
2023-05-09 23:20:00 +0700

    add new script DV_TC.008-019

---
+++ Object Repository/division/print_btn.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>print_btn</name>
+   <tag></tag>
+   <elementGuidId>9f4b5a7d-5059-44bb-99c2-e83e3deb7041</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//img[@alt='print']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
